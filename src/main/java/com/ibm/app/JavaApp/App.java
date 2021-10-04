db62c776e8a3f22968aecabbcc747ad7bf6a9565
NaveenAWSDevOps <44623515+NaveenAWSDevOps@users.noreply.github.com>
2021-10-04 11:12:09 +0530

    Update App.java

--- src/main/java/com/ibm/app/JavaApp/App.java
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Devops Training" );
+        System.out.println( "Hello World! Devops Training and Cloud Solutions" );
     }
 }
