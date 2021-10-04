001c5088d77acc2ec97170b533c53e89657a6cf8
NaveenAWSDevOps <44623515+NaveenAWSDevOps@users.noreply.github.com>
2021-10-04 11:22:22 +0530

    Update App.java

--- src/main/java/com/ibm/app/JavaApp/App.java
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Devops Training and Cloud Solutions" );
+        System.out.println( "Hello World! Devops Training and Cloud Solutions, enabled webhooks" );
     }
 }
