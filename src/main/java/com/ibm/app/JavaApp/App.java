cd8aeea05202fe69cfd1b337fe93ce16169c4b4b
NaveenAWSDevOps <44623515+NaveenAWSDevOps@users.noreply.github.com>
2021-10-05 10:08:25 +0530

    Update App.java

--- src/main/java/com/ibm/app/JavaApp/App.java
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Devops Training and Cloud Solutions, enabled webhooks" );
+        System.out.println( "Distributed Architecture set up. Worked on master/slave configuration" );
     }
 }
