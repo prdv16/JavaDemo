fc6a9c619946123e9c9c96e159836ce0109e8ef8
prdv16 <101333346+prdv16@users.noreply.github.com>
2022-03-23 16:10:24 +0530

    Update App.java

--- src/main/java/com/ibm/app/JavaApp/App.java
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Devops Training and Cloud Solutions, enabled webhooks" );
+        System.out.println( "Hello World! enabled webhooks" );
         
     }
 }
